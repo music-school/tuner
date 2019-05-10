3a1ff811cc0a72eaa9e17d64815c606ad9998d24
agrigorenko <techgriss@gmail.com>
2019-05-10 03:53:51 +0300

    Updates

---
+++ precache-manifest.d6771668672fd5d55d3147e480a1d69d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0bfdd7a3c549434189cea165576a2e",
+    "url": "/tuner/index.html"
+  },
+  {
+    "revision": "4c3ac09666cf296c93cd",
+    "url": "/tuner/static/js/2.094c5a31.chunk.js"
+  },
+  {
+    "revision": "c079df963fd4d8c304c7",
+    "url": "/tuner/static/js/main.fef30d2a.chunk.js"
+  },
+  {
+    "revision": "60fcc6b94cf3a471afd7",
+    "url": "/tuner/static/js/runtime~main.693f2c8e.js"
+  }
+]);
